d620bd1e27fd64bd0b2138e3530c36921e2068d9
Sanjeev Kumar <sanjiv@ttecherudite.com>
2023-06-27 17:19:54 +0530

    setup of medico project

---
+++ src/redux/reducer/counter.js
@@ -0,0 +1,21 @@
+import { SET_COMMENT_DATA } from "../constant";
+
+const initialState = {
+  commentData: null,
+};
+
+export default function setCommentDataReducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_COMMENT_DATA:
+      let commentData = action?.data;
+
+      console.log(commentData, "commentData");
+      return {
+        ...state,
+        commentData: commentData,
+      };
+
+    default:
+      return state;
+  }
+}
